9d857920544bfbacb2899f2ab2ff1f50e7df5d43
Ubuntu <ubuntu@ip-172-31-36-100.eu-west-1.compute.internal>
2021-04-13 22:31:00 +0000

    test

--- testBash.sh
+++ testBash.sh
@@ -1,6 +1,14 @@
-getent passwd {0..65535} | md5sum | cut -d: -f1,6 > ~/environment/testFolder/testOutput.md5   # outputs the above in md5
-# if md5sum --status -c ~/environment/testFolder/testOutput.md5; then
-#     echo OK
-# else
-#     echo FALSE
-# fi
+#!/bin/bash
+chdate=`date +"%d/%m/%y-%T"`
+# chdate=`date +"%d/%m/%y-%H:%M"`
+
+getent passwd {0..65535} | cut -d: -f1,6 > ~/environment/testFolder/current_users
+
+if md5sum --status -c ~/environment/testFolder/current_sum.md5; then
+    echo "Users not changed" > /dev/tty
+else
+    echo "Users not changed" > /dev/tty
+    echo "Hash changed on: $chdate" >> ~/environment/testFolder/current_changes
+fi
+
+md5sum ~/environment/testFolder/current_users > ~/environment/testFolder/current_sum.md5
