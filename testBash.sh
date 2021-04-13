11fb9d38deb6087f0f52a7a88d4658398d3e03b8
Ubuntu <ubuntu@ip-172-31-36-100.eu-west-1.compute.internal>
2021-04-13 22:39:37 +0000

    changes

--- testBash.sh
+++ testBash.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 chdate=`date +"%d/%m/%y-%T"`
-# chdate=`date +"%d/%m/%y-%H:%M"`
 
-getent passwd {0..65535} | cut -d: -f1,6 > ~/environment/testFolder/current_users
+#need to create some files here
+
+getent passwd {0..65535} | cut -d: -f1,6 > ~/environment/testFolder/current_users.txt
 
 if md5sum --status -c ~/environment/testFolder/current_sum.md5; then
-    echo "Users not changed" > /dev/tty
+    echo "OK: Users not changed" > /dev/tty
 else
-    echo "Users not changed" > /dev/tty
-    echo "Hash changed on: $chdate" >> ~/environment/testFolder/current_changes
+    echo "FALSE: Users changed" > /dev/tty
+    echo "Hash changed on: $chdate" >> ~/environment/testFolder/current_changes.txt
 fi
 
-md5sum ~/environment/testFolder/current_users > ~/environment/testFolder/current_sum.md5
+md5sum ~/environment/testFolder/current_users.txt > ~/environment/testFolder/current_sum.md5
